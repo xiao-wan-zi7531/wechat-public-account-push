f5fb049242169318d302af541654388e369b87e3
xiao-wan-zi7531 <112311448+xiao-wan-zi7531@users.noreply.github.com>
2022-08-28 16:48:04 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,9 +11,9 @@ export const config = {
      
     // 信息配置
     // 所在省份
-    province: "河南",
+    province: "湖北",
     // 所在城市
-    city: "驻马店",
+    city: "武汉",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
       {"name": "老婆", "year": "1996", "date": "08-21", "type": 'new'},
